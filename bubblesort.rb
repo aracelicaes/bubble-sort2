5c18cee84be2f857b6e89aca4cdcbe498f97e109
aracelicaes <aracelicaes@gmail.com>
2020-05-12 17:09:50 -0500

    Created bubble Sort part 1 with while loops and if stmts

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 
-arr = [4,3,2,5,0,2]
+p arr = [4,3,1,5,0,2]
 # arr = [4,3,78]
 
 # A method takes an array as argument
@@ -9,22 +9,43 @@ arr = [4,3,2,5,0,2]
 # If value on the left is bigger than the value on the right, they swap their positions
 # The loop will stop when no changes are made
 
-def bs(array)
-    p sorted_array = array.clone
-    change = true
-    while change do
-        sorted_array.each_with_index do |e,index|
-                if e > array[index+1]
-                    puts sorted_array[index] = array[index+1]
-                    puts sorted_array[index+1] = array[index]
-                    p sorted_array
-                end
-        end
-        change = false
+# def bs(array)
+#     p sorted_array = array.clone
+#     change = true
+#     while change do
+#         sorted_array.each_with_index do |e,index|
+#                 if e > array[index+1]
+#                     puts sorted_array[index] = array[index+1]
+#                     puts sorted_array[index+1] = array[index]
+#                     p sorted_array
+#                 end
+#         end
+#         change = false
+#     end
+#     puts 'last'
+#     p sorted_array
+# end
+#
+
+def bubblesort(arr)
+  sorted_arr = arr.clone
+  change = true
+  while change do
+    i = 0
+    count = 0  # counts the # of changes everytime the if goes thru it'll add 1
+    while i < sorted_arr.length-1
+      if sorted_arr[i] > sorted_arr[i+1]
+        count += 1
+      # switch the values here
+        sorted_arr[i], sorted_arr[i + 1] = sorted_arr[i + 1], sorted_arr[i]
+        p sorted_arr
+      end
+      i += 1
     end
-    puts 'last'
-    p sorted_array
+    change = false unless count > 0
+  end
+  p sorted_arr
 end
 
-bs(arr)
+bubblesort(arr)
 
